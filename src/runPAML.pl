d8acdc2cfe82534d5a84db7d69f742aee8eb2987
Migun Shakya <microbeatic@gmail.com>
2018-01-17 17:23:03 -0700

    tab delimition

--- src/runPAML.pl
+++ src/runPAML.pl
@@ -36,42 +36,53 @@ if ($NSsites=~/,/){
 
 opendir(DIR,$genedir);
 foreach my $files(sort{$a cmp $b}readdir(DIR)){
-   if ($files=~ /(.+)\.fna$/){
+   if ($files=~ /(.+)\.cdn$/){
       $seqfile=$genedir.'/'.$files;
       my ($name,$path,$suf)=fileparse("$seqfile",qr/\.[^.]*/);
       $output=$pamldir.'/'.$name.'.'.$suffix;
       print "$output\n";
 
       open (OUT, "> $pamldir/codeml.ctl")||die "$!";
-      print OUT "      seqfile = $seqfile * sequence data filename
-     treefile = $tree * tree structure file name
-      outfile = $output  * main result file name
+      print OUT "
+      seqfile  = $seqfile   * sequence data filename
+      treefile = $tree      * tree structure file name
+      outfile  = $output    * main result file name
 
-        noisy = 9  * 0,1,2,3,9: how much rubbish on the screen
-      verbose = 1  * 0: concise; 1: detailed, 2: too much
-      runmode = 0  * 0: user tree;  1: semi-automatic;  2: automatic
+         noisy = 9  * 0,1,2,3,9: how much rubbish on the screen
+       verbose = 1  * 0: concise; 1: detailed, 2: too much
+       runmode = 0  * 0: user tree;  1: semi-automatic;  2: automatic
                    * 3: StepwiseAddition; (4,5):PerturbationNNI; -2: pairwise
 
-      seqtype = 1  * 1:codons; 2:AAs; 3:codons-->AAs
-    CodonFreq = 2  * 0:1/61 each, 1:F1X4, 2:F3X4, 3:codon table
+       seqtype = 1  * 1:codons; 2:AAs; 3:codons-->AAs
+     CodonFreq = 2  * 0:1/61 each, 1:F1X4, 2:F3X4, 3:codon table
 
-*        ndata = 10
-        clock = 0  * 0:no clock, 1:clock; 2:local clock; 3:CombinedAnalysis
-       aaDist = 0  * 0:equal, +:geometric; -:linear, 1-6:G1974,Miyata,c,p,v,a
-   aaRatefile = dat/jones.dat  * only used for aa seqs with model=empirical(_F)
+*        ndata = 10 * number of gene alignments to be analysed
+         clock = 0  * 0:no clock, 1:clock; 2:local clock; 3:CombinedAnalysis
+        aaDist = 0  * 0:equal, +:geometric; -:linear, 1-6:G1974,Miyata,c,p,v,a
+    aaRatefile = dat/jones.dat  * only used for aa seqs with model=empirical(_F)
                    * dayhoff.dat, jones.dat, wag.dat, mtmam.dat, or your own
 
-        model = $model
-                   * models for codons:
-                       * 0:one, 1:b, 2:2 or more dN/dS ratios for branches
-                   * models for AAs or codon-translated AAs:
-                       * 0:poisson, 1:proportional, 2:Empirical, 3:Empirical+F
-                       * 6:FromCodon, 7:AAClasses, 8:REVaa_0, 9:REVaa(nr=189)
+         model = $model * 0:one omega ratio for all branches 
+                        * models for codons:
+                        * 0:one, 1:b, 2:2 or more dN/dS ratios for branches
+                        * models for AAs or codon-translated AAs:
+                        * 0:poisson, 1:proportional, 2:Empirical, 3:Empirical+F
+                        * 6:FromCodon, 7:AAClasses, 8:REVaa_0, 9:REVaa(nr=189)
 
-      NSsites = $NSsites  * 0:one w;1:neutral;2:selection; 3:discrete;4:freqs;
-                   * 5:gamma;6:2gamma;7:beta;8:beta&w;9:beta&gamma;
-                   * 10:beta&gamma+1; 11:beta&normal>1; 12:0&2normal>1;
-                   * 13:3normal>0
+      NSsites = $NSsites *  0:one omega ratio
+                         *  1:neutral
+                         *  2:selection
+                         *  3:discrete
+                         *  4:freqs;
+                         *  5:gamma
+                         *  6:2gamma
+                         *  7:beta
+                         *  8:beta & w
+                         *  9:beta&gamma
+                         * 10:beta&gamma+1 
+                         * 11:beta&normal>1
+                         * 12:0&2normal>1
+                         * 13:3normal>0
 
         icode = 0  * 0:universal code; 1:mammalian mt; 2-10:see below
         Mgene = 0
@@ -80,7 +91,7 @@ foreach my $files(sort{$a cmp $b}readdir(DIR)){
 
     fix_kappa = 0  * 1: kappa fixed, 0: kappa to be estimated
         kappa = 2  * initial or fixed kappa
-    fix_omega = 0  * 1: omega or omega_1 fixed, 0: estimate
+    fix_omega = 0  * 1: omega fixed, 0: omega to be estimate
         omega = 0.4 * initial or fixed omega, for codons or codon-based AAs
 
     fix_alpha = 1  * 0: estimate gamma shape parameter; 1: fix it at alpha
@@ -101,7 +112,7 @@ foreach my $files(sort{$a cmp $b}readdir(DIR)){
 * 7: euplotid mt., 8: alternative yeast nu. 9: ascidian mt.,
 * 10: blepharisma nu.
 * These codes correspond to transl_table 1 to 11 of GENEBANK.";
-      system("/users/252891/scratch/tools/paml/codeml");
+      system("codeml");
    }
 }
 closedir(DIR);
