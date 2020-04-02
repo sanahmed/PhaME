7ae708bd2f3da6093d5d6136abe7d15bf8b70afe
Migun Shakya <microbeatic@gmail.com>
2020-04-01 21:21:48 -0600

    fixed bootstrap bug

--- lib/PhaME.pm
+++ lib/PhaME.pm
@@ -593,6 +593,7 @@ sub buildTree {
     my $thread = shift;
     my $tree   = shift;
     my $name   = shift;
+    my $bsignal = shift; # signal showing if bootstrap should be done or not.
     my $bootstrap = shift;
     my $error  = shift;
     my $log    = shift;
@@ -616,11 +617,25 @@ sub buildTree {
             # = "raxmlHPC-PTHREADS -p 10 -T $thread -m GTRGAMMAI -s $outdir/$name\_snp_alignment.fna -w $outdir -n $name 2>>$error >> $log\n\n";
         print OUT $raxml;
         if ( system($raxml) ) { die "Error running $raxml.\n"; }
+        if ( $bsignal == 1 ) {
+            open( OUT, ">>$log" );
+            my $bootTrees
+                = "raxmlHPC-PTHREADS -p 10 -T $thread -m GTRGAMMAI -b 10000 -t $outdir/RAxML_bestTree.$name -s $outdir/$name\_all_alignment.fna -w $outdir -N $bootstrap -n $name\_b -k 2>>$error >> $log\n\n";
+            print OUT $bootTrees;
+            if ( system($bootTrees) ) { die "Error running $bootTrees.\n"; }
+            my $bestTree
+            = "raxmlHPC-PTHREADS -p 10 -T $thread -f b -m GTRGAMMAI -t $outdir/RAxML_bestTree.$name -s $outdir/$name\_all_alignment.fna -z $outdir/RAxML_bootstrap.$name\_b -w $outdir -n $name\_best 2>>$error >> $log\n\n";
+            print OUT $bestTree;
+            if ( system($bestTree) ) { die "Error running $bestTree.\n"; }
+
+        return "Bootstrap complete";
+        close OUT;
+    }
     }
 
     if ( $tree == 3 || $tree == 4 ) {
         print OUT "Reconstructing phylogeny using IQ-tree after finding the best model\n";
-        if ($bootstrap > 0){
+        if ($bsignal == 1){
             print OUT "Also bootstraping IQ-Trees trees\n";
             print OUT "\n";
             my $iqtree
